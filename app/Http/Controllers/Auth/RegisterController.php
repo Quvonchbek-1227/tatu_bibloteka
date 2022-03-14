1fb19496836e27d952324fb89bc3da933d12b947
Quvonchbek-1227 <75905122+Quvonchbek-1227@users.noreply.github.com>
2022-03-15 01:03:10 +0300

    'rrrr'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Role;
 
 class RegisterController extends Controller
 {
@@ -64,10 +65,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = $data['name'];
+        Role::create([
+            'role' => 'admin',
+            'role_desc' => 'asosiy admin'
+        ]);
+        
+        if ($user == 'admin_admin') {
+            return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role_id' => 1
         ]);
+        }
+        
     }
 }
